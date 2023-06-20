04e820d0b6e681b1fe2b39065047cbf714574778
Natalia Kirieieva <nataliakireewa@gmail.com>
2023-06-20 16:41:48 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello.</Text>
+      <Text>Is it me you're looking for ? </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f59',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
